4788a47c549179a0ca8220b283e11874defbd3cf
Anton Brashkov <spone161@gmail.com>
2023-08-13 20:50:19 +0300

    add mediaquery

--- src/components/Container.styled.tsx
+++ src/components/Container.styled.tsx
@@ -5,9 +5,15 @@ type ContainerPropsType = {
 }
 export const Container = styled.div<ContainerPropsType>`
   display: flex;
+  //flex-wrap: wrap;
   gap: 40px;
   justify-content: center;
   align-items: center;
   height: 100vh;
   background: ${props => props.bg || '#fff'};
+  padding: 1rem;
+  
+  @media screen and (max-width: 765px) {
+    flex-direction: column;
+  }
 `
--- src/components/ImagesCard.styled.tsx
+++ src/components/ImagesCard.styled.tsx
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const ImagesCard = styled.img`
   border-radius: 11px;
+  max-width: 100%;
+  width: 280px;
+  object-fit: cover;
 `
